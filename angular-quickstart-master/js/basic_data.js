2d88ea39a646f401970e7c4ca573973ce92525a1
Seveny <544392498@qq.com>
2017-04-03 01:04:05 +0800

    试卷基础信息

--- angular-quickstart-master/js/basic_data.js
+++ angular-quickstart-master/js/basic_data.js
@@ -31,7 +31,109 @@
                 controller: 'contentProduceController'
             }
         },
-        API_URL:' http://localhost:3000'
+        API_URL:' http://localhost:3000',
+        PAPER_TYPE:[
+            {
+                code:'',
+                value:'请选择'
+            },
+            {
+                code:'11',
+                value:'高考真题'
+            },
+            {
+                code:'12',
+                value:'中考真题'
+            },
+            {
+                code:'13',
+                value:'小升初真题'
+            },
+            {
+                code:'14',
+                value:'期中考试'
+            },
+            {
+                code:'15',
+                value:'期末考试'
+            },
+            {
+                code:'16',
+                value:'模拟考试'
+            },
+            {
+                code:'17',
+                value:'联考'
+            },
+            {
+                code:'18',
+                value:'月考'
+            },
+            {
+                code:'19',
+                value:'单元测试'
+            }],
+        YEAR:['请选择',2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006,2005,2004,2003,2002,2001,2000],
+        SUBJECT:[
+            {
+               code:'',
+                value:'请选择'
+            },
+            {
+                code:'21',
+                value:'语文'
+            },
+            {
+                code:'22',
+                value:'数学'
+            },
+            {
+                code:'23',
+                value:'英语'
+            }
+        ],
+        GRADE:[
+            {
+                code: '',
+                value:'请选择'
+            },
+            {
+                code: '31',
+                value:'一年级'
+            },
+            {
+                code: '32',
+                value:'二年级'
+            },
+            {
+                code: '33',
+                value:'三年级'
+            },
+            {
+                code: '34',
+                value:'四年级'
+            },
+            {
+                code: '35',
+                value:'五年级'
+            },
+            {
+                code: '36',
+                value:'六年级'
+            },
+            {
+                code: '37',
+                value:'初一'
+            },
+            {
+                code: '38',
+                value:'初二'
+            },
+            {
+                code: '39',
+                value:'初三'
+            }
+        ]
     };
     window.BASIC_DATA=BASIC_DATA;
 })();
--- angular-quickstart-master/js/controllers/resource_upload.js
+++ angular-quickstart-master/js/controllers/resource_upload.js
@@ -3,11 +3,28 @@
  */
 (function () {
     angular.module('myApp').controller('resourceUploadController',['$scope','$http','$location',function ($scope,$http,$location) {
-        declare($scope,$location);
+        declare($scope);
         init($scope,$http);
     }]);
-    function declare($scope,$location) {
-
+    function declare($scope) {
+        $scope.viewController={
+            paperType_change:function (selectedType) {
+                $scope.paperInfo.paperType=selectedType;
+                console.log($scope.paperInfo);
+            },
+            year_change:function (selectedYear) {
+                $scope.paperInfo.year=selectedYear;
+                console.log($scope.paperInfo)
+            },
+            subject_change:function (selectedSubject) {
+                $scope.paperInfo.subject=selectedSubject;
+                console.log($scope.paperInfo);
+            },
+            grade_change:function (selectedGrade) {
+                $scope.paperInfo.grade=selectedGrade;
+                console.log($scope.paperInfo);
+            }
+        }
     }
     function init($scope,$http) {
         //配置jQuery文件上传插件
@@ -15,7 +32,19 @@
             showThumbs:true,
             addMore:true
         });
-        $scope.nimab='a'
+        $scope.paperType=BASIC_DATA.PAPER_TYPE;
+        $scope.year=BASIC_DATA.YEAR;
+        $scope.subject=BASIC_DATA.SUBJECT;
+        $scope.grade=BASIC_DATA.GRADE;
+        $scope.paperInfo={
+            paperType:'',
+            year:'',
+            region:'',
+            subject:'',
+            grade:'',
+            school:'',
+            paperName:''
+        }
     }
 })();
 
